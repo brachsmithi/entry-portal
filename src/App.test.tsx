2bcd079cbf5d6c2d1bd4ee37ae24eb5057f9f60e
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-04 16:20:31 -0400

    add paginated navigation to app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+
+  it('renders pagination nav', () => {
+    render(<App />)
+    const element = screen.getByText(/Page 1 of 1/i)
+    expect(element).toBeInTheDocument()
+  })
+
+})
