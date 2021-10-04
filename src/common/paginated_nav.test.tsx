e8855b441ebebf45777174646a4d66ebc33d886e
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-04 16:08:23 -0400

    add total pages to paginated nav

--- src/common/paginated_nav.test.tsx
+++ src/common/paginated_nav.test.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { PaginatedNav } from './paginated_nav'
+import { defaultPaginationMetadata } from "../models/PaginationMetadata";
 
 describe('PaginatedNav', () => {
 
   it('renders with empty metadata', () => {
-    render(<PaginatedNav />)
+    render(<PaginatedNav metadata={defaultPaginationMetadata} />)
     const currentPageDisplay = screen.getByText(/Page 1 of 1/i)
+    expect(currentPageDisplay).toBeInTheDocument()
+  })
+
+  it('renders on first page of many', () => {
+    const metadata = {
+      ...defaultPaginationMetadata,
+      totalPages: 90
+    };
+    render(<PaginatedNav metadata={metadata} />)
+    const currentPageDisplay = screen.getByText(/Page 1 of 90/i)
     expect(currentPageDisplay).toBeInTheDocument()
   })
 
