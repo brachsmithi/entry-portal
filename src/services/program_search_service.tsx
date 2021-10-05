8f4fb91b8a2d92f7d850b34824cda48ef88e86d9
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-05 19:17:34 -0400

    add page parameter to programs loading

--- src/services/program_search_service.tsx
+++ src/services/program_search_service.tsx
@@ -1,7 +1,9 @@
-import { PaginatedPrograms } from "../models/PaginatedPrograms"
+import { PaginatedPrograms } from '../models/PaginatedPrograms'
 
-export default async function loadPrograms(): Promise<PaginatedPrograms> {
-  const response = await fetch("http://localhost:3000")
+export default async function loadPrograms(page?: number): Promise<PaginatedPrograms> {
+  let url = 'http://localhost:3000'
+  if (page) url += `?page=${page}`
+  const response = await fetch(url)
       .then(response => response.json())
   return {
     paginationMetadata: {
