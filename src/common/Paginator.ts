26ae987b902d4208c0d72efa62cc4d7dd830fbc0
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-10 11:34:40 -0400

    add data loading to paginator

--- src/common/Paginator.ts
+++ src/common/Paginator.ts
@@ -1,10 +1,33 @@
+import ProgramSearchResponse from "../models/ProgramSearchResponse"
+import { emptyPaginatedPrograms } from "../models/PaginatedPrograms"
+import PaginationMetadata, { defaultPaginationMetadata } from "../models/PaginationMetadata"
+
 export default class Paginator {
 
+  private readonly loadMethod: (page?: number) => Promise<ProgramSearchResponse>
+  private paginationMetadata: PaginationMetadata
+
+  constructor(loadMethod: (page?: number) => Promise<ProgramSearchResponse>) {
+    this.loadMethod = loadMethod
+    this.paginationMetadata = defaultPaginationMetadata
+  }
+
   currentPage() {
-    return 1;
+    return 1
   }
 
   totalPages() {
-    return 1;
+    return this.paginationMetadata.totalPages
+  }
+
+  async load() {
+    return await this.loadMethod()
+        .then((response: ProgramSearchResponse) => {
+          return response.paginatedPrograms ?? emptyPaginatedPrograms
+        })
+        .then((paginatedData) => {
+          this.paginationMetadata = paginatedData.paginationMetadata
+          return []
+        })
   }
 }
