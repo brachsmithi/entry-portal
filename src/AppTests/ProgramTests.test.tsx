be3d9f75cb499a9924087c2bd971d46dcf291a0b
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-12 19:50:01 -0400

    enable next page loading

---
+++ src/AppTests/ProgramTests.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from '../App'
+import { returnJson } from "../testhelpers/ProgramSearchJson"
+import userEvent from "@testing-library/user-event"
+
+describe('Program Integration', () => {
+
+  beforeEach(() => {
+    // @ts-ignore
+    fetch.resetMocks()
+  })
+
+  it('renders pagination nav', async () => {
+    const currentPage = 1
+    // @ts-ignore
+    fetch.mockResponseOnce(returnJson(currentPage))
+    render(<App />)
+    // @ts-ignore
+    expect(fetch.mock.calls[0][0]).toEqual("http://localhost:3000/programs.json?page=1")
+    expect(await screen.findByText(/Page 1/)).toBeInTheDocument()
+
+    // @ts-ignore
+    fetch.mockResponseOnce(returnJson(currentPage+1))
+    let element = await screen.findByText('Next')
+    userEvent.click(element)
+    // @ts-ignore
+    expect(fetch.mock.calls[1][0]).toEqual("http://localhost:3000/programs.json?page=2")
+    expect(await screen.findByText(/Page 2/)).toBeInTheDocument()
+
+    // @ts-ignore
+    // fetch.mockResponseOnce(returnJson(currentPage))
+    // element = await screen.findByText('Prev')
+    // userEvent.click(element)
+    // expect(fetch.mock.calls[2][0]).toEqual("http://localhost:3000/programs.json?page=1")
+    // expect(await screen.findByText(/Page 1/)).toBeInTheDocument()
+  })
+
+})
