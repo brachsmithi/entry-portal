3c565d8fe6ec635310ba995f4aa008fc8caace24
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-13 18:05:28 -0400

    populate previous page in metadata read from programs json

--- src/models/PaginationMetadata.ts
+++ src/models/PaginationMetadata.ts
@@ -1,11 +1,13 @@
 export default interface PaginationMetadata {
   totalPages: number,
   currentPage: number,
-  nextPage: number
+  nextPage: number,
+  previousPage: number
 }
 
-export const defaultPaginationMetadata = {
+export const defaultPaginationMetadata: PaginationMetadata = {
   totalPages: 1,
   currentPage: 1,
-  nextPage: 1
+  nextPage: 1,
+  previousPage: 1
 }
--- src/services/ProgramSearchService.test.tsx
+++ src/services/ProgramSearchService.test.tsx
@@ -4,13 +4,14 @@ import PaginatedData from "../models/PaginatedData"
 
 describe('ProgramSearchService', () => {
 
-  function expected(currentPage: number, nextPage: number): PaginatedData {
+  function expected(currentPage: number, nextPage: number, previousPage: number): PaginatedData {
     return {
       data: [],
       paginationMetadata: {
         currentPage: currentPage,
         totalPages: totalPages,
-        nextPage: nextPage
+        nextPage: nextPage,
+        previousPage: previousPage
       }
     }
   }
@@ -23,37 +24,40 @@ describe('ProgramSearchService', () => {
   it('loads first page of paginated program data from local service', async () => {
     const currentPage = 1
     const nextPage = 2
+    const previousPage = 1
     // @ts-ignore
     fetch.mockResponseOnce(returnJson(currentPage))
 
     const response = await loadPrograms()
 
     expect(fetch).toHaveBeenCalledWith("http://localhost:3000/programs.json")
-    expect(response.paginatedData).toEqual(expected(currentPage, nextPage))
+    expect(response.paginatedData).toEqual(expected(currentPage, nextPage, previousPage))
   })
 
   it('loads requested page of paginated program data from local service', async () => {
     const currentPage = 45
     const nextPage = 46
+    const previousPage = 44
     // @ts-ignore
     fetch.mockResponseOnce(returnJson(currentPage))
 
     const response = await loadPrograms(currentPage)
 
     expect(fetch).toHaveBeenCalledWith(`http://localhost:3000/programs.json?page=${currentPage}`)
-    expect(response.paginatedData).toEqual(expected(currentPage, nextPage))
+    expect(response.paginatedData).toEqual(expected(currentPage, nextPage, previousPage))
   })
 
   it('loads last page of paginated program data from local service', async () => {
     const currentPage = totalPages
     const nextPage = totalPages
+    const previousPage = totalPages - 1
     // @ts-ignore
     fetch.mockResponseOnce(returnJson(currentPage))
 
     const response = await loadPrograms(currentPage)
 
     expect(fetch).toHaveBeenCalledWith(`http://localhost:3000/programs.json?page=${currentPage}`)
-    expect(response.paginatedData).toEqual(expected(currentPage, nextPage))
+    expect(response.paginatedData).toEqual(expected(currentPage, nextPage, previousPage))
   })
 
 })
