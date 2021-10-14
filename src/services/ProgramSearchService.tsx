08b2ecdf3612c6ff7d4a64ca22775fddce0d84fb
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-14 18:53:41 -0400

    populate listings from program json

--- src/services/ProgramSearchService.tsx
+++ src/services/ProgramSearchService.tsx
@@ -1,15 +1,27 @@
 import ProgramSearchResponse from "../models/ProgramSearchResponse"
 import SearchResponse from "../models/SearchResponse"
+import { ListingData } from "../models/ListingData";
 
 export default async function loadPrograms(page?: number): Promise<SearchResponse> {
   let url = 'http://localhost:3000/programs.json'
   if (page) url += `?page=${page}`
   const response = await fetch(url)
       .then(response => response.json())
+
+  function listing(program: any): ListingData {
+    return {
+      name: program.title
+    }
+  }
+
+  function programListings(programs: any[]): Array<ListingData> {
+    return programs.map(program => listing(program));
+  }
+
   return new ProgramSearchResponse(
       {
         data: {
-          data: [],
+          data: programListings(response.programs),
           paginationMetadata: {
             currentPage: response.pagination_metadata.current_page,
             nextPage: response.pagination_metadata.next_page,
