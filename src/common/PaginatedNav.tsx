6c56e52e256de2cd920b584f5b0649ba3f96d489
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-15 16:43:45 -0400

    add stylesheet for paginated nav

--- src/common/PaginatedNav.tsx
+++ src/common/PaginatedNav.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './PaginatedNav.css'
 import PaginationMetadata from "../models/PaginationMetadata"
 
 interface PaginatedNavProps {
@@ -9,17 +10,17 @@ interface PaginatedNavProps {
 
 export function PaginatedNav(props: PaginatedNavProps): JSX.Element {
   return (
-      <div>
+      <div className='paginatedNav'>
         { props.metadata.currentPage > 1 &&
-          <div>
+          <div className='navButton'>
             <button onClick={props.previousAction}>Prev</button>
           </div>
         }
-        <div>
+        <div className='location'>
           Page {props.metadata.currentPage} of {props.metadata.totalPages}
         </div>
         { props.metadata.totalPages > props.metadata.currentPage &&
-          <div>
+          <div className='navButton'>
             <button onClick={props.nextAction}>Next</button>
           </div>
         }
---
+++ src/stories/PaginatedNav.stories.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { PaginatedNav } from "../common/PaginatedNav"
+import "../common/PaginatedNav.css"
+import { defaultPaginationMetadata } from "../models/PaginationMetadata"
+
+export default {
+  title: 'PaginatedNav',
+  component: PaginatedNav,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof PaginatedNav>
+
+const Template: ComponentStory<typeof PaginatedNav> = (args) => <PaginatedNav {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  metadata: defaultPaginationMetadata,
+  previousAction: () => {},
+  nextAction: () => {}
+}
+
+export const FirstOfMany = Template.bind({})
+FirstOfMany.args = {
+  metadata: {
+    ...defaultPaginationMetadata,
+    currentPage: 1,
+    totalPages: 50
+  },
+  previousAction: () => {},
+  nextAction: () => {}
+}
+
+export const MidstOfMany = Template.bind({})
+MidstOfMany.args = {
+  metadata: {
+    ...defaultPaginationMetadata,
+    currentPage: 26,
+    totalPages: 50
+  },
+  previousAction: () => {},
+  nextAction: () => {}
+}
+
+export const LastOfMany = Template.bind({})
+LastOfMany.args = {
+  metadata: {
+    ...defaultPaginationMetadata,
+    currentPage: 50,
+    totalPages: 50
+  },
+  previousAction: () => {},
+  nextAction: () => {}
+}
