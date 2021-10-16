8477bac03e183f24fef030242efdb3b960c38dd1
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-16 18:11:02 -0400

    add program json for testing

---
+++ src/testhelpers/ProgramJson.ts
@@ -0,0 +1,34 @@
+export function returnProgramJson() {
+  return `
+    {
+      "id": 2193,
+      "title": "Kidnapped",
+      "year": "1974",
+      "version": "Lamberto Bava Reconstruction",
+      "series": [],
+      "length_in_minutes": 96,
+      "alternate_titles": [
+        {
+          "name":"Wild Dogs"
+        }
+      ],
+      "people": [
+        {
+          "name": "Mario Bava",
+          "aliases": [
+            {
+              "name": "John M. Old"
+            },
+            {
+              "name": "John Hold"
+            }
+          ]
+        },
+        {
+          "name": "Lamberto Bava",
+          "aliases": []
+        }
+      ]
+    }
+  `
+}
