1555e0c17d8bd5cba6877ddabf59b95795e1a3be
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-16 20:22:24 -0400

    add look up for program data by id

--- src/services/ProgramSearchService.test.tsx
+++ src/services/ProgramSearchService.test.tsx
@@ -1,4 +1,4 @@
-import loadProgramListings from './ProgramSearchService'
+import loadProgramListings, { loadProgramDetails } from './ProgramSearchService'
 import {
   totalProgramPages,
   returnProgramListingJson,
@@ -8,68 +8,91 @@ import {
   programListing4
 } from "../testhelpers/ProgramSearchJson"
 import PaginatedData from "../models/PaginatedData"
+import { programData, returnProgramJson } from "../testhelpers/ProgramJson";
 
 describe('ProgramSearchService', () => {
 
-  function expected(currentPage: number, nextPage: number, previousPage: number): PaginatedData {
-    return {
-      data: [
-        programListing1,
-        programListing2,
-        programListing3,
-        programListing4
-      ],
-      paginationMetadata: {
-        currentPage: currentPage,
-        totalPages: totalProgramPages,
-        nextPage: nextPage,
-        previousPage: previousPage
+  describe('loadProgramListings', () => {
+
+    function expected(currentPage: number, nextPage: number, previousPage: number): PaginatedData {
+      return {
+        data: [
+          programListing1,
+          programListing2,
+          programListing3,
+          programListing4
+        ],
+        paginationMetadata: {
+          currentPage: currentPage,
+          totalPages: totalProgramPages,
+          nextPage: nextPage,
+          previousPage: previousPage
+        }
       }
     }
-  }
 
-  beforeEach(() => {
-    // @ts-ignore
-    fetch.resetMocks()
+    beforeEach(() => {
+      // @ts-ignore
+      fetch.resetMocks()
+    })
+
+    it('loads first page of paginated program data from local service', async () => {
+      const currentPage = 1
+      const nextPage = 2
+      const previousPage = 1
+      // @ts-ignore
+      fetch.mockResponseOnce(returnProgramListingJson(currentPage))
+
+      const response = await loadProgramListings()
+
+      expect(fetch).toHaveBeenCalledWith("http://localhost:3000/programs.json")
+      expect(response.paginatedData).toEqual(expected(currentPage, nextPage, previousPage))
+    })
+
+    it('loads requested page of paginated program data from local service', async () => {
+      const currentPage = 45
+      const nextPage = 46
+      const previousPage = 44
+      // @ts-ignore
+      fetch.mockResponseOnce(returnProgramListingJson(currentPage))
+
+      const response = await loadProgramListings(currentPage)
+
+      expect(fetch).toHaveBeenCalledWith(`http://localhost:3000/programs.json?page=${currentPage}`)
+      expect(response.paginatedData).toEqual(expected(currentPage, nextPage, previousPage))
+    })
+
+    it('loads last page of paginated program data from local service', async () => {
+      const currentPage = totalProgramPages
+      const nextPage = totalProgramPages
+      const previousPage = totalProgramPages - 1
+      // @ts-ignore
+      fetch.mockResponseOnce(returnProgramListingJson(currentPage))
+
+      const response = await loadProgramListings(currentPage)
+
+      expect(fetch).toHaveBeenCalledWith(`http://localhost:3000/programs.json?page=${currentPage}`)
+      expect(response.paginatedData).toEqual(expected(currentPage, nextPage, previousPage))
+    })
+
   })
 
-  it('loads first page of paginated program data from local service', async () => {
-    const currentPage = 1
-    const nextPage = 2
-    const previousPage = 1
-    // @ts-ignore
-    fetch.mockResponseOnce(returnProgramListingJson(currentPage))
+  describe('loadProgramDetails', () => {
 
-    const response = await loadProgramListings()
+    beforeEach(() => {
+      // @ts-ignore
+      fetch.resetMocks()
+    })
 
-    expect(fetch).toHaveBeenCalledWith("http://localhost:3000/programs.json")
-    expect(response.paginatedData).toEqual(expected(currentPage, nextPage, previousPage))
-  })
+    it('loads the requested program from local service', async () => {
+      // @ts-ignore
+      fetch.mockResponseOnce(returnProgramJson())
 
-  it('loads requested page of paginated program data from local service', async () => {
-    const currentPage = 45
-    const nextPage = 46
-    const previousPage = 44
-    // @ts-ignore
-    fetch.mockResponseOnce(returnProgramListingJson(currentPage))
+      const response = await loadProgramDetails(programData.id)
 
-    const response = await loadProgramListings(currentPage)
-
-    expect(fetch).toHaveBeenCalledWith(`http://localhost:3000/programs.json?page=${currentPage}`)
-    expect(response.paginatedData).toEqual(expected(currentPage, nextPage, previousPage))
-  })
-
-  it('loads last page of paginated program data from local service', async () => {
-    const currentPage = totalProgramPages
-    const nextPage = totalProgramPages
-    const previousPage = totalProgramPages - 1
-    // @ts-ignore
-    fetch.mockResponseOnce(returnProgramListingJson(currentPage))
-
-    const response = await loadProgramListings(currentPage)
-
-    expect(fetch).toHaveBeenCalledWith(`http://localhost:3000/programs.json?page=${currentPage}`)
-    expect(response.paginatedData).toEqual(expected(currentPage, nextPage, previousPage))
+      expect(fetch).toHaveBeenCalledWith(`http://localhost:3000/programs/${programData.id}.json`)
+      expect(response.programData).toEqual(programData)
+    })
   })
 
 })
--- src/services/ProgramSearchService.tsx
+++ src/services/ProgramSearchService.tsx
@@ -1,5 +1,9 @@
 import PaginatedSearchResponse from "../models/PaginatedSearchResponse"
 import { ListingData } from "../models/ListingData"
+import ProgramResponse from "../models/ProgramResponse"
+import AlternateTitle from "../models/AlternateTitle"
+import Person from "../models/Person";
+import Alias from "../models/Alias";
 
 export default async function loadProgramListings(page?: number): Promise<PaginatedSearchResponse> {
   let url = 'http://localhost:3000/programs.json'
@@ -39,6 +43,58 @@ export default async function loadProgramListings(page?: number): Promise<Pagina
             previousPage: response.pagination_metadata.previous_page,
             totalPages: response.pagination_metadata.total_pages
           }
+        }
+      }
+  )
+}
+
+export async function loadProgramDetails(id: number): Promise<ProgramResponse> {
+  let url = `http://localhost:3000/programs/${id}.json`
+  const response = await fetch(url)
+      .then(response => response.json())
+
+  function alternateTitle(title: any): AlternateTitle {
+    return {
+      name: title.name
+    }
+  }
+
+  function alternateTitles(alternateTitles: any[]): Array<AlternateTitle> {
+    return alternateTitles.map((title) => alternateTitle(title))
+  }
+
+  function personAlias(alias: any): Alias {
+    return {
+      name: alias.name
+    }
+  }
+
+  function personAliases(aliases: any[]): Array<Alias> {
+    return aliases.map((alias) => personAlias(alias));
+  }
+
+  function programPerson(person: any): Person {
+    return {
+      name: person.name,
+      aliases: personAliases(person.aliases)
+    };
+  }
+
+  function people(people: any[]): Array<Person> {
+    return people.map((person) => programPerson(person));
+  }
+
+  return new ProgramResponse(
+      {
+        data: {
+          id: response.id,
+          title: response.title,
+          year: response.year,
+          version: response.version,
+          lengthInMinutes: response.length_in_minutes,
+          series: [],
+          alternateTitles: alternateTitles(response.alternate_titles),
+          people: people(response.people)
         }
       }
   )
