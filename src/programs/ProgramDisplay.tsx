b5d49e2cad4c6db47870de869741fcf858f7802d
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-17 18:04:21 -0400

    fill out non-array program data on display

--- src/programs/ProgramDisplay.tsx
+++ src/programs/ProgramDisplay.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ProgramData from "../models/ProgramData";
+import { formatDuration } from "../common/DisplayFormatters"
 
 export interface ProgramDisplayProperties {
   program: ProgramData
@@ -9,6 +10,9 @@ export function ProgramDisplay(props: ProgramDisplayProperties): JSX.Element {
   return (
       <div className='program-display'>
         <div className='title'>{props.program.title}</div>
+        <div className='year'>{props.program.year}</div>
+        <div className='version'>{props.program.version}</div>
+        <div className='duration'>{formatDuration(props.program.lengthInMinutes)}</div>
       </div>
   )
 }
