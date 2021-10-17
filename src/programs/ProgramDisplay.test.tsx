4b780ebf7930c2168785fcc42c6242843a1f74ad
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-17 12:34:47 -0400

    create program display

---
+++ src/programs/ProgramDisplay.test.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { ProgramDisplay } from "./ProgramDisplay";
+import ProgramData from "../models/ProgramData";
+
+describe('ProgramDisplay', () => {
+
+  it('shows details of fully filled program', () => {
+    const program: ProgramData = {
+      id: 223,
+      title: 'The Film\'s Title',
+      year: '2021',
+      version: 'TV Edit',
+      lengthInMinutes: 87,
+      people: [
+        {
+          name: 'Director One',
+          aliases: [
+            {
+              name: 'D. One'
+            },
+            {
+              name: 'Director Uno'
+            }
+          ]
+        },
+        {
+          name: 'Director Two',
+          aliases: [
+            {
+              name: 'D. Two'
+            },
+            {
+              name: 'Director Dos'
+            }
+          ]
+        }
+      ],
+      series: [
+        'The Film Series',
+        'The Unofficial Film Series'
+      ],
+      alternateTitles: [
+        {
+          name: 'The Title of the Film'
+        },
+        {
+          name: 'Title: The Film'
+        }
+      ]
+    }
+    render(<ProgramDisplay program={program}/>)
+
+    expect(screen.queryByText('The Film\'s Title')).toBeInTheDocument()
+  })
+
+})
