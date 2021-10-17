05a248ed1a59a98b9114150232e66bdb4bd703f9
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-16 20:26:52 -0400

    refactor program json test data

--- src/testhelpers/ProgramJson.ts
+++ src/testhelpers/ProgramJson.ts
@@ -1,6 +1,6 @@
 import ProgramData from "../models/ProgramData"
 
-export const programData: ProgramData = {
+export const programData1: ProgramData = {
   id: 2193,
   title: 'Kidnapped',
   year: '1974',
@@ -31,37 +31,35 @@ export const programData: ProgramData = {
   ]
 }
 
-export function returnProgramJson() {
-  return `
-    {
-      "id": 2193,
-      "title": "Kidnapped",
-      "year": "1974",
-      "version": "Lamberto Bava Reconstruction",
-      "series": [],
-      "length_in_minutes": 96,
-      "alternate_titles": [
-        {
-          "name":"Wild Dogs"
-        }
-      ],
-      "people": [
-        {
-          "name": "Mario Bava",
-          "aliases": [
-            {
-              "name": "John M. Old"
-            },
-            {
-              "name": "John Hold"
-            }
-          ]
-        },
-        {
-          "name": "Lamberto Bava",
-          "aliases": []
-        }
-      ]
-    }
-  `
-}
+export const programJson1: string = `
+  {
+    "id": 2193,
+    "title": "Kidnapped",
+    "year": "1974",
+    "version": "Lamberto Bava Reconstruction",
+    "series": [],
+    "length_in_minutes": 96,
+    "alternate_titles": [
+      {
+        "name":"Wild Dogs"
+      }
+    ],
+    "people": [
+      {
+        "name": "Mario Bava",
+        "aliases": [
+          {
+            "name": "John M. Old"
+          },
+          {
+            "name": "John Hold"
+          }
+        ]
+      },
+      {
+        "name": "Lamberto Bava",
+        "aliases": []
+      }
+    ]
+  }
+`
