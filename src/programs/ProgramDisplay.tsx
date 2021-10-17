024a628730a4ac3cba328f24526f7547ad58bed6
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-17 18:59:09 -0400

    add people and aliases to program display

--- src/programs/ProgramDisplay.tsx
+++ src/programs/ProgramDisplay.tsx
@@ -1,18 +1,44 @@
 import React from 'react'
 import ProgramData from "../models/ProgramData";
 import { formatDuration } from "../common/DisplayFormatters"
+import Person from "../models/Person"
+import Alias from "../models/Alias";
 
 export interface ProgramDisplayProperties {
   program: ProgramData
 }
 
 export function ProgramDisplay(props: ProgramDisplayProperties): JSX.Element {
+  const peopleElements = (personArray: Array<Person>) => {
+    function personElement(person: any, index: number) {
+      return (
+          <div key={index} className='person'>
+            <span className='main'>
+              {person.name}
+            </span>
+            <span className='alias'>
+              {person.aliases.map((alias: Alias) => alias.name).join('/')}
+            </span>
+          </div>
+      )
+    }
+
+    const personElements = personArray.map((person, index) => {
+      return personElement(person, index)
+    })
+    return (
+        <div className='people'>
+          {personElements}
+        </div>
+    )
+  }
   return (
       <div className='program-display'>
         <div className='title'>{props.program.title}</div>
         <div className='year'>{props.program.year}</div>
         <div className='version'>{props.program.version}</div>
         <div className='duration'>{formatDuration(props.program.lengthInMinutes)}</div>
+        <div>{peopleElements(props.program.people)}</div>
       </div>
   )
 }
