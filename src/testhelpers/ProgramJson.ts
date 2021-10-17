94a4979b8b9916d7028c5a484d514798dd457a21
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-16 20:43:35 -0400

    load series into program data

--- src/testhelpers/ProgramJson.ts
+++ src/testhelpers/ProgramJson.ts
@@ -63,3 +63,51 @@ export const programJson1: string = `
     ]
   }
 `
+
+export const programData2: ProgramData = {
+  id: 2070,
+  title: 'You Only Live Twice',
+  year: '1967',
+  version: '',
+  series: [
+      'James Bond (Eon)',
+      'James Bond (Connery)'
+  ],
+  lengthInMinutes: 97,
+  alternateTitles: [
+    {
+      name: 'Ian Fleming\'s You Only Live Twice'
+    }
+  ],
+  people: [
+    {
+      name: 'Lewis Gilbert',
+      aliases: []
+    }
+  ]
+}
+
+export const programJson2: string = `
+  {
+    "id": 2070,
+    "title": "You Only Live Twice",
+    "year": "1967",
+    "version": "",
+    "series": [
+      "James Bond (Eon)",
+      "James Bond (Connery)"
+    ],
+    "length_in_minutes": 97,
+    "alternate_titles": [
+      {
+        "name":"Ian Fleming's You Only Live Twice"
+      }
+    ],
+    "people": [
+      {
+        "name":"Lewis Gilbert",
+        "aliases": []
+      }
+    ]
+  }
+ `
