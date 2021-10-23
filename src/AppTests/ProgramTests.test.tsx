f5b5621ee1ca8b512b653a2d16696f0a03b5e669
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-23 13:38:38 -0400

    add routing for program details

--- src/AppTests/ProgramTests.test.tsx
+++ src/AppTests/ProgramTests.test.tsx
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react'
 import App from '../App'
 import { returnProgramListingJson } from "../testhelpers/ProgramSearchJson"
 import userEvent from "@testing-library/user-event"
+import { programData2, programJson2 } from "../testhelpers/ProgramJson"
 
 describe('Program Integration', () => {
 
@@ -11,7 +12,7 @@ describe('Program Integration', () => {
     fetch.resetMocks()
   })
 
-  it('renders pagination nav', async () => {
+  it('includes pagination nav', async () => {
     const currentPage = 1
     // @ts-ignore
     fetch.mockResponseOnce(returnProgramListingJson(currentPage))
@@ -35,6 +36,21 @@ describe('Program Integration', () => {
     // @ts-ignore
     expect(fetch.mock.calls[2][0]).toEqual("http://localhost:3000/programs.json?page=1")
     expect(await screen.findByText(/Page 1/)).toBeInTheDocument()
+  })
+
+  it('loads program from link', async () => {
+    // @ts-ignore
+    fetch.mockResponseOnce(programJson2)
+    // const history = createMemoryHistory()
+    // history.push(`/programs/${programData2.id}`)
+    window.history.pushState({}, '', `/programs/${programData2.id}`)
+    render(
+          <App />
+    )
+
+    expect(await screen.findByText(programData2.title)).toBeInTheDocument()
+    expect(await screen.findByText(programData2.version)).toBeInTheDocument()
+    expect(await screen.findByText(programData2.alternateTitles[0].name)).toBeInTheDocument()
   })
 
 })
