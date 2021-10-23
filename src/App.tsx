3bb7b2a35b3c7700001733f46a9a13d033bde1b9
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-23 13:38:38 -0400

    add routing for program details

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import {
     BrowserRouter as Router,
     Route,
-    Switch
+    Switch,
+    useParams,
+    useRouteMatch
 } from "react-router-dom"
 import { PaginatedProgramsScreen } from "./programs/PaginatedProgramsScreen"
+import { ProgramDisplay } from "./programs/ProgramDisplay"
+import { loadProgramDetails } from "./services/ProgramSearchService"
+import { emptyProgramData } from "./models/ProgramData"
 
 function App() {
   return (
       <Router>
         <Switch>
+          <Route path='/programs'>
+            <Programs/>
+          </Route>
           <Route path='/'>
             <ProgramList/>
           </Route>
@@ -21,6 +29,33 @@ function App() {
 
 export default App
 
+function Programs() {
+  let match = useRouteMatch()
+  return (
+    <Switch>
+      <Route path={`${match.path}/:programId`}>
+        <ProgramDetail/>
+      </Route>
+      <Route path={match.path}>
+        <ProgramList/>
+      </Route>
+    </Switch>
+  )
+}
+
 function ProgramList() {
   return <PaginatedProgramsScreen/>
+}
+
+function ProgramDetail() {
+  // @ts-ignore
+  let { programId } = useParams()
+  const [program, setProgram] = useState(emptyProgramData)
+  useEffect(() => {
+    loadProgramDetails(programId).then((response) => {
+      setProgram(response.programData)
+    })
+  }, [programId, setProgram])
+
+  return <ProgramDisplay program={program}/>
 }
