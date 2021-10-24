77f4a661e8ed467e103ca7b84a47e7a28d003d0f
brachsmithi <bsmithi.impaired@gmail.com>
2021-10-24 12:03:23 -0400

    attach function to back button

--- src/common/DetailNav.test.tsx
+++ src/common/DetailNav.test.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import { render, screen } from "@testing-library/react"
 import DetailNav from "./DetailNav"
+import userEvent from "@testing-library/user-event"
 
 describe('DetailNav', () => {
 
-  it('should have back button', () => {
-    render(<DetailNav/>)
+  it('should have default buttons', () => {
+    render(<DetailNav backAction={jest.fn}/>)
 
     expect(screen.getByText('Back')).toBeEnabled()
+  })
+
+  it('uses provided callback for back action', () => {
+    const backFunction = jest.fn()
+    render(<DetailNav backAction={backFunction} />)
+
+    const backElement = screen.getByText('Back')
+    userEvent.click(backElement)
+
+    expect(backFunction).toHaveBeenCalled()
   })
 
 })
