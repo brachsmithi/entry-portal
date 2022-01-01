be150dd7e80754410333da0bd7499ea3766ea872
brachsmithi <bsmithi.impaired@gmail.com>
2022-01-01 11:35:20 -0500

    search field requires 3 characters to auto fire provided action

--- src/common/SearchField.test.tsx
+++ src/common/SearchField.test.tsx
@@ -1,12 +1,25 @@
-import { render } from "@testing-library/react";
-import { SearchField } from "./SearchField";
+import { render } from "@testing-library/react"
+import { SearchField } from "./SearchField"
+import userEvent from "@testing-library/user-event"
 
 describe('SearchField', () => {
 
   it('renders a text field', () => {
-    const {getByPlaceholderText} = render(<SearchField/>)
+    const {getByPlaceholderText} = render(<SearchField action={jest.fn()}/>)
     const element = getByPlaceholderText('Enter search text')
     expect(element).toBeInTheDocument()
     expect(element).toHaveAttribute('id', 'searchField')
+  })
+
+  it('fires action when there are 3 letters in search text', () => {
+    const action = jest.fn()
+    const {getByPlaceholderText} = render(<SearchField action={action}/>)
+    const element = getByPlaceholderText('Enter search text')
+
+    userEvent.type(element, 'ab')
+    expect(action).not.toHaveBeenCalled()
+
+    userEvent.type(element, 'c')
+    expect(action).toHaveBeenCalledWith('abc')
   })
 })
--- src/common/SearchField.tsx
+++ src/common/SearchField.tsx
@@ -1,12 +1,22 @@
+import { ChangeEvent } from "react"
+
 interface SearchFieldProps {
+  action: (searchTerm: string) => {}
 }
 
 export function SearchField(props: SearchFieldProps): JSX.Element {
+  const callAction = (event: ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = event.target.value
+    if (searchTerm.length >= 3) {
+      props.action(searchTerm)
+    }
+  }
   return (
      <input
          id='searchField'
          type='text'
          placeholder='Enter search text'
+         onChange={callAction}
      />
   )
 }
