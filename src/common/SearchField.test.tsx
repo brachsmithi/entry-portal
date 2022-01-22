933e311388b8e14450729b44f375daf1d1f98786
brachsmithi <bsmithi.impaired@gmail.com>
2022-01-22 17:31:37 -0500

    search field creates list items for each search result

--- src/common/SearchField.test.tsx
+++ src/common/SearchField.test.tsx
@@ -1,25 +1,69 @@
-import { render } from "@testing-library/react"
+import { act, render } from "@testing-library/react"
 import { SearchField } from "./SearchField"
 import userEvent from "@testing-library/user-event"
+import SearchTermResponse from "../models/SearchTermResponse"
 
 describe('SearchField', () => {
 
   it('renders a text field', () => {
-    const {getByPlaceholderText} = render(<SearchField action={jest.fn()}/>)
-    const element = getByPlaceholderText('Enter search text')
-    expect(element).toBeInTheDocument()
-    expect(element).toHaveAttribute('id', 'searchField')
+    const { getByRole } = render(<SearchField action={jest.fn()}/>)
+    const textbox = getByRole('textbox') as HTMLInputElement
+    expect(textbox.placeholder).toEqual('Enter search text')
+    expect(textbox).toHaveAttribute('id', 'searchField')
   })
 
-  it('fires action when there are 3 letters in search text', () => {
-    const action = jest.fn()
-    const {getByPlaceholderText} = render(<SearchField action={action}/>)
-    const element = getByPlaceholderText('Enter search text')
+  it('fires action when there are 3 letters in search text', async () => {
+    const action = jest.fn((_: string) => Promise.resolve(new SearchTermResponse({})))
+    const { getByRole } = render(<SearchField action={action}/>)
+    const textbox = getByRole('textbox')
 
-    userEvent.type(element, 'ab')
+    userEvent.type(textbox, 'ab')
     expect(action).not.toHaveBeenCalled()
 
-    userEvent.type(element, 'c')
+    await act(() => userEvent.type(textbox, 'c'))
+
     expect(action).toHaveBeenCalledWith('abc')
+  })
+
+  it('displays list of options matching search text', async () => {
+    const searchTerm = 'disco'
+    const response = new SearchTermResponse({
+      data: {
+        data: [
+          {
+            id: 5463,
+            primary: 'Disco Godfather',
+            secondary: ['1979'],
+            tertiary: []
+          },
+          {
+            id: 5065,
+            primary: 'Star Trek VI: The Undiscovered Country',
+            secondary: ['1991'],
+            tertiary: ['Star Trek (TOS Films)']
+          }
+        ],
+        searchMetadata: {
+          searchTerm: searchTerm,
+          resultCount: 2
+        }
+      }
+    })
+    // create mock function
+    const action = jest.fn()
+    // attach return to mock function
+    action.mockResolvedValue(Promise.resolve(response))
+    // create search field with hooks
+    const { getByRole } = render(<SearchField action={action}/>)
+    // retrieve field from hook
+    const textbox = getByRole('textbox')
+    // fill in field with search text
+    await act(() => userEvent.type(textbox, searchTerm))
+    expect(action).toHaveBeenCalledWith(searchTerm)
+    // retrieve list from hook
+    const list = getByRole('listbox')
+    // verify contents of list
+    const options = list.getElementsByTagName('li')
+    expect(options.length).toEqual(2)
   })
 })
--- src/common/SearchField.tsx
+++ src/common/SearchField.tsx
@@ -1,22 +1,46 @@
-import { ChangeEvent } from "react"
+import { ChangeEvent, useState } from "react"
+import SearchTermResponse from "../models/SearchTermResponse"
+import { ListingData } from "../models/ListingData"
 
 interface SearchFieldProps {
-  action: (searchTerm: string) => {}
+  action: (searchTerm: string) => Promise<SearchTermResponse>
 }
 
+interface OptionLinkData {
+  text: string
+}
+
+const emptyOptionLinks: Array<OptionLinkData> = []
+
 export function SearchField(props: SearchFieldProps): JSX.Element {
+  const [optionLinks, setOptionLinks] = useState(emptyOptionLinks)
+  const createOptionLinkData = (_: ListingData) => {
+    return {
+      text: "foo"
+    }
+  }
   const callAction = (event: ChangeEvent<HTMLInputElement>) => {
     const searchTerm = event.target.value
     if (searchTerm.length >= 3) {
       props.action(searchTerm)
+          .then(result => {
+            const options = result.data.data.map(listing => createOptionLinkData(listing))
+            setOptionLinks(options)
+          })
     }
   }
+  const options = (linkData: Array<OptionLinkData>) => linkData.map((data, i) => {
+    return <li key={i}>{data.text}</li>
+  })
   return (
-     <input
-         id='searchField'
-         type='text'
-         placeholder='Enter search text'
-         onChange={callAction}
-     />
+      <>
+        <input
+            id='searchField'
+            type='text'
+            placeholder='Enter search text'
+            onChange={callAction}
+        />
+        <ul role='listbox'>{options(optionLinks)}</ul>
+      </>
   )
 }
