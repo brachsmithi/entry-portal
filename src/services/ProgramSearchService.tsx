5285f0b01d2591551b316e814cfdb5814db3b94f
brachsmithi <bsmithi.impaired@gmail.com>
2022-01-27 20:40:06 -0500

    extract common inner function from program queries

--- src/services/ProgramSearchService.tsx
+++ src/services/ProgramSearchService.tsx
@@ -4,32 +4,13 @@ import ProgramResponse from "../models/ProgramResponse"
 import AlternateTitle from "../models/AlternateTitle"
 import Person from "../models/Person"
 import Alias from "../models/Alias"
-import SearchTermResponse from "../models/SearchTermResponse";
+import SearchTermResponse from "../models/SearchTermResponse"
 
 export default async function loadProgramListings(page?: number): Promise<PaginatedSearchResponse> {
   let url = 'http://localhost:3000/programs.json'
   if (page) url += `?page=${page}`
   const response = await fetch(url)
       .then(response => response.json())
-
-  function listing(program: any): ListingData {
-    const secondaryValues: string[] = []
-    const pushIfHasValue = (array: string[], value?: string) => {
-      if (value && value !== '') array.push(value)
-    }
-    pushIfHasValue(secondaryValues, program.year)
-    pushIfHasValue(secondaryValues, program.version)
-    const tertiaryValues: string[] = []
-    program.series.forEach((series: string)=>{
-      pushIfHasValue(tertiaryValues, series)
-    })
-    return {
-      id: program.id,
-      primary: program.title,
-      secondary: secondaryValues,
-      tertiary: tertiaryValues
-    }
-  }
 
   function programListings(programs: any[]): Array<ListingData> {
     return programs.map(program => listing(program));
@@ -48,6 +29,33 @@ export default async function loadProgramListings(page?: number): Promise<Pagina
         }
       }
   )
+}
+
+export async function loadProgramSearchResults(searchTerm: string, page?: number): Promise<SearchTermResponse> {
+  let url = `http://localhost:3000/programs.json?search=${searchTerm}`
+  if (page) url += `&page=${page}`
+  const response = await fetch(url)
+      .then(response => response.json())
+
+  function searchListings(programs: any[]): Array<ListingData> {
+    return programs.map(program => listing(program));
+  }
+
+  return new SearchTermResponse({
+    data: {
+      data: searchListings(response.programs),
+      searchMetadata: {
+        resultCount: response.search_metadata.current_programs,
+        searchTerm: searchTerm
+      },
+      paginationMetadata: {
+        currentPage: response.pagination_metadata.current_page,
+        nextPage: response.pagination_metadata.next_page,
+        previousPage: response.pagination_metadata.previous_page,
+        totalPages: response.pagination_metadata.total_pages
+      }
+    }
+  })
 }
 
 export async function loadProgramDetails(id: number): Promise<ProgramResponse> {
@@ -102,48 +110,21 @@ export async function loadProgramDetails(id: number): Promise<ProgramResponse> {
   )
 }
 
-export async function loadProgramSearchResults(searchTerm: string, page?: number): Promise<SearchTermResponse> {
-  let url = `http://localhost:3000/programs.json?search=${searchTerm}`
-  if (page) url += `&page=${page}`
-  const response = await fetch(url)
-      .then(response => response.json())
-
-  function listing(program: any): ListingData {
-    const secondaryValues: string[] = []
-    const pushIfHasValue = (array: string[], value?: string) => {
-      if (value && value !== '') array.push(value)
-    }
-    pushIfHasValue(secondaryValues, program.year)
-    pushIfHasValue(secondaryValues, program.version)
-    const tertiaryValues: string[] = []
-    program.series.forEach((series: string)=>{
-      pushIfHasValue(tertiaryValues, series)
-    })
-    return {
-      id: program.id,
-      primary: program.title,
-      secondary: secondaryValues,
-      tertiary: tertiaryValues
-    }
+function listing(program: any): ListingData {
+  const secondaryValues: string[] = []
+  const pushIfHasValue = (array: string[], value?: string) => {
+    if (value && value !== '') array.push(value)
   }
-
-  function searchListings(programs: any[]): Array<ListingData> {
-    return programs.map(program => listing(program));
-  }
-
-  return new SearchTermResponse({
-    data: {
-      data: searchListings(response.programs),
-      searchMetadata: {
-        resultCount: response.search_metadata.current_programs,
-        searchTerm: searchTerm
-      },
-      paginationMetadata: {
-        currentPage: response.pagination_metadata.current_page,
-        nextPage: response.pagination_metadata.next_page,
-        previousPage: response.pagination_metadata.previous_page,
-        totalPages: response.pagination_metadata.total_pages
-      }
-    }
+  pushIfHasValue(secondaryValues, program.year)
+  pushIfHasValue(secondaryValues, program.version)
+  const tertiaryValues: string[] = []
+  program.series.forEach((series: string)=>{
+    pushIfHasValue(tertiaryValues, series)
   })
+  return {
+    id: program.id,
+    primary: program.title,
+    secondary: secondaryValues,
+    tertiary: tertiaryValues
+  }
 }
