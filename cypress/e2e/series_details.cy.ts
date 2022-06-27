0856c53f4608972b3d95ebf72499a371337b059a
brachsmithi <bsmithi.impaired@gmail.com>
2022-06-27 19:12:25 -0400

    replace integration test for series details with e2e test

---
+++ cypress/e2e/series_details.cy.ts
@@ -0,0 +1,17 @@
+import { seriesWithWrapperAndDiscsData } from '../../src/testhelpers/SeriesJson'
+
+describe('Series Details', () => {
+
+  it('loads series details from the server', () => {
+    cy.intercept('GET',
+        `http://localhost:3000/series/${seriesWithWrapperAndDiscsData.id}`)
+
+    cy.visit(`/series/${seriesWithWrapperAndDiscsData.id}`)
+
+    cy.contains(seriesWithWrapperAndDiscsData.name)
+    cy.contains(seriesWithWrapperAndDiscsData.wrapperSeries[0].name)
+    cy.contains(seriesWithWrapperAndDiscsData.discs[0].name)
+    cy.contains(seriesWithWrapperAndDiscsData.discs[0].sequence)
+  })
+
+})
