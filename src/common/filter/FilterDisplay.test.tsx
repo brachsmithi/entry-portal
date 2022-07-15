c76ece6a4d4dfca886ba3f740a090954da5bc03f
brachsmithi <bsmithi.impaired@gmail.com>
2022-07-15 13:35:05 -0400

    display filter search or filter clear depending on state of filtration

--- src/common/filter/FilterDisplay.test.tsx
+++ src/common/filter/FilterDisplay.test.tsx
@@ -1,6 +1,6 @@
 import FilterStrategy from '../../models/FilterStrategy'
 import { DetailLinkAction } from '../detail/DetailLinkAction'
-import { render } from '@testing-library/react'
+import { ByRoleMatcher, ByRoleOptions, Matcher, SelectorMatcherOptions, render } from '@testing-library/react'
 import FilterDisplay from './FilterDisplay'
 import { FilterType } from '../../services/FilterType'
 import DataResponse from '../../models/DataResponse'
@@ -8,24 +8,77 @@ import FilterData from '../../models/FilterData'
 
 describe('FilterDisplay', () => {
 
-  it('filter description and clear button', () => {
-    const id = 24
-    const filterStrategy: FilterStrategy = {
-      filterAction: (_, __) => {
-        return Promise.resolve(new DataResponse<FilterData>({}))
+  describe('when filtering', () => {
+
+    let roleGetter: (text: ByRoleMatcher, options?: (ByRoleOptions | undefined), waitForElementOptions?: unknown) => HTMLElement
+    let textGetter: (text: Matcher, options?: (SelectorMatcherOptions | undefined), waitForElementOptions?: unknown) => HTMLElement
+    let roleQuerier: (text: ByRoleMatcher, options?: (ByRoleOptions | undefined), waitForElementOptions?: unknown) => (HTMLElement | null)
+
+    beforeEach(() => {
+      const id = 24
+      const filterStrategy: FilterStrategy = {
+        filterAction: (_, __) => {
+          return Promise.resolve(new DataResponse<FilterData>({}))
+        }
       }
-    }
-    const linkAction: DetailLinkAction = {
-      loadAction: _ => {},
-      rootPath: '/filter'
-    }
-    const {getByRole, getByText} = render(
-        <FilterDisplay filterStrategy={filterStrategy} linkAction={linkAction} filterType={FilterType.Program} id={id}/>
-    )
-    expect(getByText('Filter on program')).toBeInTheDocument()
-    const link = getByRole('link')
-    expect(link).toHaveAttribute('href', '/filter')
-    expect(link).toHaveTextContent('Clear')
+      const linkAction: DetailLinkAction = {
+        loadAction: _ => {},
+        rootPath: '/filter'
+      }
+      const {getByRole, getByText, queryByRole} = render(
+          <FilterDisplay filterStrategy={filterStrategy} linkAction={linkAction} filterType={FilterType.Program} id={id}/>
+      )
+      roleGetter = getByRole
+      textGetter = getByText
+      roleQuerier = queryByRole
+    })
+
+    it('presents filter description and clear button', () => {
+      expect(textGetter('Filter on program')).toBeInTheDocument()
+      const link = roleGetter('link')
+      expect(link).toHaveAttribute('href', '/filter')
+      expect(link).toHaveTextContent('Clear')
+    })
+
+    it('does not contain program search form', () => {
+      expect(roleQuerier('textbox')).not.toBeInTheDocument()
+    })
+
+  })
+
+  describe('when filter is not set', () => {
+
+    let roleGetter: (text: ByRoleMatcher, options?: (ByRoleOptions | undefined), waitForElementOptions?: unknown) => HTMLElement
+    let textQuerier: (text: Matcher, options?: (SelectorMatcherOptions | undefined), waitForElementOptions?: unknown) => (HTMLElement | null)
+    let roleQuerier: (text: ByRoleMatcher, options?: (ByRoleOptions | undefined), waitForElementOptions?: unknown) => (HTMLElement | null)
+
+    beforeEach(() => {
+      const filterStrategy: FilterStrategy = {
+        filterAction: (_, __) => {
+          return Promise.resolve(new DataResponse<FilterData>({}))
+        }
+      }
+      const linkAction: DetailLinkAction = {
+        loadAction: _ => {},
+        rootPath: '/filter'
+      }
+      const {getByRole, queryByRole, queryByText} = render(
+          <FilterDisplay filterStrategy={filterStrategy} linkAction={linkAction} filterType={FilterType.Program}/>
+      )
+      roleGetter = getByRole
+      roleQuerier = queryByRole
+      textQuerier = queryByText
+    })
+
+    it('presents program search form', () => {
+      expect(roleGetter('textbox')).toHaveAttribute('placeholder', 'Enter text')
+    })
+
+    it('does not provide filter description and clear button', () => {
+      expect(textQuerier('Filter on program')).not.toBeInTheDocument()
+      expect(roleQuerier('link')).not.toBeInTheDocument()
+    })
+
   })
 
 })
--- src/common/filter/FilterDisplay.tsx
+++ src/common/filter/FilterDisplay.tsx
@@ -2,23 +2,48 @@ import './FilterDisplay.css'
 import FilterStrategy from '../../models/FilterStrategy'
 import { DetailLinkAction } from '../detail/DetailLinkAction'
 import { FilterType } from '../../services/FilterType'
+import PicklistInputField from '../component/PicklistInputField'
+import DataResponse from "../../models/DataResponse";
+import SearchData from "../../models/SearchData";
 
 export interface FilterDisplayProps {
   filterStrategy: FilterStrategy
   linkAction: DetailLinkAction
   filterType: FilterType
-  id: number
+  id?: number
 }
 
 export default function FilterDisplay(props: FilterDisplayProps) {
+  const termAction = (_: string) => {
+    return new Promise<DataResponse<SearchData>>(_ => new DataResponse<SearchData>({}))
+  }
+  const loadAction = () => {
+
+  }
+  const setTerm = () => {
+
+  }
+
   return(
       <div className='filter'>
-        <div className='description'>
-          Filter on {props.filterType.toLowerCase()}
-        </div>
-        <div className='clear-button'>
-          <a href={props.linkAction.rootPath}>Clear</a>
-        </div>
+        { props.id &&
+          <div>
+            <div className='description'>
+              Filter on {props.filterType.toLowerCase()}
+            </div>
+            <div className='clear-button'>
+              <a href={props.linkAction.rootPath}>Clear</a>
+            </div>
+          </div>
+        }
+
+        { !props.id &&
+          <div>
+            <PicklistInputField
+                termAction={termAction}
+                loadAction={loadAction}
+                setTerm={setTerm}/>
+          </div> }
       </div>
   )
 }
